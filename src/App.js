26ac5cb7539a5c6566372b7180f5a84ee684ef3a
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-06-02 17:27:38 -0400

    create a ShopPage component from our SHOP_DATA

--- src/App.js
+++ src/App.js
@@ -2,23 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
-
-const HatsPage = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <h1>Hats Page</h1>
-    </div>
-  )
-}
-
+import ShopPage from './pages/shop/shop.component'
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/shop/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
 
     </div>
