272dbbfda2c8a45d37d4f53450371414c6f4401a
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-06-18 15:38:25 -0400

    implement redux-firebase user authentification

--- src/App.js
+++ src/App.js
@@ -1,38 +1,34 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+
 import './App.css';
-import Header from './components/header/header.component'
-import HomePage from './pages/homepage/homepage.component'
-import ShopPage from './pages/shop/shop.component'
+import Header from './components/header/header.component';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
-import { auth, createUserProfileDocument } from './firebase/firebase.utils'
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/users.actions'
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      currentUser: null
-    };
-  }
-
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+    const { setCurrentUser } = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data()
-            }
-          });
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
+          })
         });
-      } else {
-        this.setState({ currentUser: userAuth })
       }
+
+      setCurrentUser(userAuth)
     })
   }
 
@@ -43,7 +39,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
@@ -55,4 +51,8 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(null, mapDispatchToProps)(App);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import {connect} from 'react-redux';
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 
@@ -29,5 +30,10 @@ const Header = ({ currentUser }) => (
     </div>
 );
 
+const mapStateToProps = (state) => ({
+    currentUser: state.user.currentUser
 
-export default Header;
+})
+
+
+export default connect(mapStateToProps)(Header);
