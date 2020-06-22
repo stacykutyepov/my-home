3cdf51ced76abcb2622e4e89c64da779cf6b21fc
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-06-22 15:04:15 -0400

    add redux.persist to store cart items

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import store from './redux/store'
+import { store, persistor } from './redux/store'
 
 import './index.css';
 import App from './App';
@@ -13,7 +14,9 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
