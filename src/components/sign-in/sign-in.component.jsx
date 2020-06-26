7dfc0bf9d4e0d2041c20c58f97153eeca969566e
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-06-26 10:46:21 -0400

    fixed a typo on sign-in page

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -36,7 +36,7 @@ class SignIn extends React.Component {
         return (
             <div className="sign-in">
                 <h2>I already have an account</h2>
-                <span>Sign in with your eamil and password</span>
+                <span>Sign in with your email and password</span>
 
                 <form onSubmit={this.handleSubmit}>
                     <FormInput
