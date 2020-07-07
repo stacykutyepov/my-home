c896421bca2198fada8202c9b555da115050f0ac
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-07-07 15:34:04 -0400

    add Collections to firestore db

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -37,6 +37,40 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 
     return userRef;
 
+};
+
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+    const collectionRef = firestore.collection(collectionKey);
+
+    const batch = firestore.batch();
+    objectsToAdd.forEach(obj => {
+        // set up unique keys 
+        const newDocRef = collectionRef.doc();
+        // set up new value
+        batch.set(newDocRef, obj);
+    });
+    // fire off our batch request, returns us a Promise
+    return await batch.commit()
+};
+
+// covert to an object, and add new properties
+export const convertCollectionSnapshotToMap = (collections) => {
+    const transformedCollection = collections.docs.map(doc => {
+        const { title, items } = doc.data();
+        return {
+            // convert symbols URL can read
+            routeName: encodeURI(title.toLowerCase()),
+            id: doc.id,
+            title,
+            items
+        }
+    });
+    // console.log(transformedCollection);
+    
+    return transformedCollection.reduce((accumulator, collection) => {
+        accumulator[collection.title.toLowerCase()] = collection;
+        return accumulator;
+    }, {})
 }
 
 
