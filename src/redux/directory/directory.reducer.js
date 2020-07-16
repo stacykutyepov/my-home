0cdc3dfc247fb0e5920780a1fa3daf8cb5ede45b
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-07-16 16:28:21 -0400

    update shop diretory with correct data

--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -1,37 +1,38 @@
 const INITIAL_STATE = {
     sections: [
         {
-            title: "hats",
-            imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+            title: "loft",
+            imageUrl: "https://i.ibb.co/rs4VWSH/loft.jpg",
             id: 1,
-            linkUrl: "shop/hats",
+            linkUrl: "shop/loft",
         },
         {
-            title: "jackets",
-            imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+            title: "minimalist",
+            imageUrl: "https://i.ibb.co/hBvDmy2/Minimalist.jpg",
             id: 2,
-            linkUrl: "shop/jackets",
+            linkUrl: "shop/minimalist",
         },
         {
-            title: "sneakers",
-            imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+            title: "craft-mortar",
+            imageUrl: "https://i.ibb.co/R9c0N2q/craft-mortat.jpg",
             id: 3,
-            linkUrl: "shop/sneakers",
+            size: "large",
+            linkUrl: "shop/craft-mortar",
         },
         {
-            title: "womens",
-            imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+            title: "colored",
+            imageUrl: "https://i.ibb.co/cNsb1yQ/Colorfull.jpg",
             size: "large",
             id: 4,
-            linkUrl: "shop/womens",
+            linkUrl: "shop/colored",
         },
-        {
-            title: "mens",
-            imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-            size: "large",
-            id: 5,
-            linkUrl: "shop/mens",
-        },
+        // {
+        //     title: "mens",
+        //     imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+        //     size: "large",
+        //     id: 5,
+        //     linkUrl: "shop/mens",
+        // },
     ],
 };
 
