88b60d023a3e5689e8bc04f908b6da82cf1503cc
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-07-18 14:32:44 -0400

    create favCollection page

---
+++ src/pages/favorite-collection/favorite-collection.component.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { connect } from "react-redux";
+import { SelectFavorite } from "../../redux/favorite/favorite.selectors";
+import { createStructuredSelector } from "reselect";
+import CollectionItem from "../../components/collection-item/collection-item";
+
+import {
+  CollectionPageContainer,
+  Tittle,
+  ItemsContainer,
+} from "./favorite-collection.styles";
+
+const FavoritePage = ({ favoriteCollection }) => {
+  console.log(favoriteCollection);
+  return (
+    <CollectionPageContainer>
+      <Tittle>favorite</Tittle>
+      <ItemsContainer>
+        {favoriteCollection.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </ItemsContainer>
+    </CollectionPageContainer>
+  );
+};
+
+// const mapStateToProps = createStructuredSelector({
+//   cartItems: selectCartItems,
+//   total: selectCartTotal,
+// });
+
+const mapStateToProps = createStructuredSelector({
+  favoriteCollection: SelectFavorite,
+});
+
+export default connect(mapStateToProps)(FavoritePage);
---
+++ src/pages/favorite-collection/favorite-collection.styles.jsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const CollectionPageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Tittle = styled.h2`
+  font-size: 38px;
+  margin: 0 auto 30px;
+`;
+
+export const ItemsContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-gap: 10px;
+
+  & > div {
+    margin-bottom: 30px;
+  }
+  
+  @media (max-width: 800px) {
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 15px;
+  }
+`;
