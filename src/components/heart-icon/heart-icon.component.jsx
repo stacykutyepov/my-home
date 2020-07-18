c8e5deaa8e77f0df4d2f0638672bd96efc90f80b
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-07-18 14:32:23 -0400

    update favorite count from redux-State

--- src/components/heart-icon/heart-icon.component.jsx
+++ src/components/heart-icon/heart-icon.component.jsx
@@ -1,26 +1,24 @@
 import React from "react";
-// import { connect } from "react-redux";
-// import { createStructuredSelector } from "reselect";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
-// import { toggleCartHidden } from "../../redux/cart/cart.actions";
-// import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
+import { SelectFavorite } from "../../redux/favorite/favorite.selectors";
 
+import {
+  HeartContainer,
+  ShoppingIcon,
+  ItemCountContainer,
+} from "./heart-icon.styles";
 
-import { HeartContainer, ShoppingIcon, ItemCountContainer } from "./heart-icon.styles";
-
-const HeartIcon = ({ onClick, itemCount }) => (
+const HeartIcon = ({ onClick, itemFavoriteCount }) => (
   <HeartContainer onClick={onClick}>
     <ShoppingIcon />
-    <ItemCountContainer>0</ItemCountContainer>
+    <ItemCountContainer>{itemFavoriteCount.length}</ItemCountContainer>
   </HeartContainer>
 );
 
-// const mapDispatchToProps = (dispath) => ({
-//   toggleCartHidden: () => dispath(toggleCartHidden()),
-// });
+const mapStateToProps = createStructuredSelector({
+  itemFavoriteCount: SelectFavorite,
+});
 
-// const mapStateToProps = createStructuredSelector({
-//   itemCount: selectCartItemsCount,
-// });
-
-export default HeartIcon;
+export default connect(mapStateToProps)(HeartIcon);
