2f235c2ff261419641e0edcd807a14497a9faee7
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-07-20 17:20:48 -0400

    react lazy and Suspense implementation

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { GlobalStyle } from './global.styles';
 
 import Header from './components/header/header.component';
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component';
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
-import CheckoutPage from './pages/checkout/checkout.component';
-import FavoritePage from './pages/favorite-collection/favorite-collection.component';
+import Spinner from './components/spinner/spinner.component';
+import ErrorBoundary from './components/error-boundary/error-boundary.component';
 
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/users.actions';
 import { selectCurrentUser } from './redux/user/user.selector';
+
+const HomePage = lazy(() => import('./pages/homepage/homepage.component'))
+const ShopPage = lazy(() => import('./pages/shop/shop.component'))
+const SignInAndSignUpPage = lazy(() => import('./pages/sign-in-and-sign-up/sign-in-and-sign-up'))
+const CheckoutPage = lazy(() => import('./pages/checkout/checkout.component'))
+const FavoritePage = lazy(() => import('./pages/favorite-collection/favorite-collection.component'))
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
@@ -45,12 +48,16 @@ class App extends React.Component {
       <div>
         <GlobalStyle />
         <Header />
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/shop' component={ShopPage} />
-          <Route exact path='/checkout' component={CheckoutPage} />
-          <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage />)} />
-          <Route exact path='/favorite' component={FavoritePage} />
+          <Switch>
+        <ErrorBoundary>
+            <Suspense fallback={<Spinner />}>
+              <Route exact path='/' component={HomePage} />
+              <Route path='/shop' component={ShopPage} />
+              <Route exact path='/checkout' component={CheckoutPage} />
+              <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage />)} />
+              <Route exact path='/favorite' component={FavoritePage} />
+            </Suspense>
+        </ErrorBoundary>
         </Switch>
 
       </div>
