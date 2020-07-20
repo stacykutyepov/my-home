1e0bd09503bb7e39a84fdb9ee70bbbe164d7378c
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-07-20 14:20:01 -0400

    change directory images URL

--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -2,37 +2,31 @@ const INITIAL_STATE = {
     sections: [
         {
             title: "loft",
-            imageUrl: "https://i.ibb.co/rs4VWSH/loft.jpg",
+            imageUrl: "https://i.ibb.co/7Nz39Rf/loft.jpg",
             id: 1,
             linkUrl: "shop/loft",
         },
         {
             title: "minimalist",
-            imageUrl: "https://i.ibb.co/hBvDmy2/Minimalist.jpg",
+            imageUrl: "https://i.ibb.co/tBQSS2H/Minimalist.jpg",
             id: 2,
             linkUrl: "shop/minimalist",
         },
         {
             title: "craft-mortar",
-            imageUrl: "https://i.ibb.co/R9c0N2q/craft-mortat.jpg",
+            imageUrl: "https://i.ibb.co/rZtrtqW/craft-mortat.jpg",
             id: 3,
             size: "large",
             linkUrl: "shop/craft-mortar",
         },
         {
             title: "colored",
-            imageUrl: "https://i.ibb.co/cNsb1yQ/Colorfull.jpg",
+            imageUrl: "https://i.ibb.co/HtGP5h4/Colorfull.jpg",
             size: "large",
             id: 4,
             linkUrl: "shop/colored",
         },
-        // {
-        //     title: "mens",
-        //     imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-        //     size: "large",
-        //     id: 5,
-        //     linkUrl: "shop/mens",
-        // },
+
     ],
 };
 
